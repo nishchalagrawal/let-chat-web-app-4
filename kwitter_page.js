c84350707a95e69e001271312bb7aa8d6d7d3ea7
nishchal agrawal <80205568+nishchalagrawal@users.noreply.github.com>
2021-10-03 11:55:50 +0530

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,20 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCtjTUJab2UtmPrgJzMRxecV2pBeSAsnAw",
+    authDomain: "let-chat-web-app-71417.firebaseapp.com",
+    databaseURL: "https://let-chat-web-app-71417-default-rtdb.firebaseio.com",
+    projectId: "let-chat-web-app-71417",
+    storageBucket: "let-chat-web-app-71417.appspot.com",
+    messagingSenderId: "131598919969",
+    appId: "1:131598919969:web:6a0eafa9eac9f529503660"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+
+  user_name = localStorage.getItem("user_name")
+  room_name = localStorage.getItem("room_name")
+
+  function send(){
+      
+  }
